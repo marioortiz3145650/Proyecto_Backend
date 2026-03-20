0dd08f7c0c33f205b71934c356a8a1c17cd3dc43
Juan Jose Rosero <juanjoserosero812@gmail.com>
2026-03-20 09:46:38 -0500

    Agregué módulo tipo_de_alimento

--- src/alimentos/entities/alimento.entity.ts
+++ src/alimentos/entities/alimento.entity.ts
@@ -1,19 +1,23 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { UnidadMedida } from '../../unidades_de_medida/entities/unidades_de_medida.entity';
+import { TipoDeAlimento } from 'src/tipo_de_alimento/entities/tipo_de_alimento.entity';
 
 @Entity('alimento')
 export class Alimento {
 
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ name: 'id_insumo' })
   id_insumo: number;
 
   @Column({ type: 'varchar', length: 100 })
   nombre: string;
 
-  @Column()
-  tipo_alimento_id: number;
+  @ManyToOne(() => TipoDeAlimento)
+  @JoinColumn({ name: 'tipo_alimento_id' })
+  tipo_alimento: TipoDeAlimento;
 
-  @Column()
-  unidad_medida_id: number;
+  @ManyToOne(() => UnidadMedida)
+  @JoinColumn({ name: 'unidad_medida_id' })
+  unidad_medida: UnidadMedida;
 
   @Column({ type: 'numeric', precision: 10, scale: 2, default: 0 })
   stock_actual: number;
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlimentosModule } from './alimentos/alimentos.module';
 import { UnidadesDeMedidaModule } from './unidades_de_medida/unidades_de_medida.module';
+import { TipoDeAlimentosModule } from './tipo_de_alimento/tipo_de_alimento.module';
 
 
 @Module({
@@ -25,6 +26,7 @@ import { UnidadesDeMedidaModule } from './unidades_de_medida/unidades_de_medida.
     RolsModule,
     AlimentosModule,
     UnidadesDeMedidaModule,
+    TipoDeAlimentosModule,
   ],
   controllers: [],
   providers: [],
